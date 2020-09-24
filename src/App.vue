b73871fd8a2bdd4d2a09664dfb173d3a1cf4fbd2
ArayaTa <68525443+ArayaTa@users.noreply.github.com>
2020-09-24 11:15:40 +0700

    enter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <anime>
+   </anime>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue' */
+import Anime from './components/Anime.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   // HelloWorld,
+    Anime
   }
 }
 </script>
